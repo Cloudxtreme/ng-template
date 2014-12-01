954f12a0528e69490a1ad76431a17ee40f4a7fa7
kethle <ktleary@gmail.com>
2014-12-01 01:58:35 -0500

    lint

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -1,40 +1,43 @@
 var testApp = angular.module('testApp', []);
 //create simple controller
-testApp.controller('testController', function($scope, zipcodeService){
-  $scope.hello="Hello There!";
+testApp.controller('testController', function ($scope, zipcodeService) {
+  $scope.hello = "Hello There!";
   $scope.testControllerTest = '';
- 
-  $scope.processAddress = function (){
+
+  $scope.processAddress = function () {
     $scope.testControllerTest = "waiting for service--";
    //build GET options -- substitute street, city, etc with your form data
     var zipData  = {
-      baseurl:'https://api.smartystreets.com/street-address',
+      baseurl: 'https://api.smartystreets.com/street-address',
       auth_id: 'f9f5b120-b95b-4dc0-81d1-f62a30c5e7b3',
-      auth_token: 'usLhmPVxda78SOCMa3eD', 
-      street: '90 Fleet St', 
-      street2: '', 
-      city: 'Portsmouth', 
-      state: 'NH', 
+      auth_token: 'usLhmPVxda78SOCMa3eD',
+      street: '90 Fleet St',
+      street2: '',
+      city: 'Portsmouth',
+      state: 'NH',
       zipcode: ''
     };
 
 
     //link this function up to ng-click or whichever event you like
     zipcodeService.returnData(zipData)
-     .success(function(data, status, headers) {
+      .success(function (data) {
+        var d, comps;
       //view object properties in console, remove later
       //console.log(data[0]);
-      
-      //replace console.log with form elements (e.g. form.zipcode = ...)
-      for (var d in data){
-        comps = data[d].components;
-        console.log(comps.zipcode);
-        console.log(comps.plus4_code);
-        console.log(comps.city_name);
-        console.log(comps.state_abbreviation);
-        $scope.testControllerTest = comps.zipcode; 
-      }
-    });
+
+        //replace console.log with form elements (e.g. form.zipcode = ...)
+        for (d in data) {
+          if (data.hasOwnProperty(d)) {
+            comps = data[d].components;
+            console.log(comps.zipcode);
+            console.log(comps.plus4_code);
+            console.log(comps.city_name);
+            console.log(comps.state_abbreviation);
+            $scope.testControllerTest = comps.zipcode;
+          }
+        }
+      });
   };
 });
 
